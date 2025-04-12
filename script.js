aacb7e29916f5f4e67a7bb6b7e082838ae1e35b7
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-04-12 15:28:26 +0100

    Style hamburger menu for mobile

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const overlay = document.getElementById("overlay");
 
 hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
     navLinks.classList.toggle("show");
+    overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("show");
+    overlay.classList.remove("show");
 });
